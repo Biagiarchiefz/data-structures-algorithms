9b95fa0cb032cd1afa6f26a7fe24b02b35943931
Biagiarchiefz <biagiiarchie@gmail.com>
2025-09-23 23:50:54 +0700

    add sorting algorithm

---
+++ src/sorting/SortingAlgortihms.java
@@ -0,0 +1,34 @@
+package sorting;
+
+public class SortingAlgortihms {
+    public static void main(String[] args) {
+
+        int[] nums = {4, 2, 5, 6, 7, 9, 8};
+        int size = nums.length;
+        int temp = 0;
+
+        System.out.println("Before sorting");
+        for (int num : nums) {
+            System.out.print(num + ", ");
+        }
+
+        for (int i = 0; i < size; i++) {
+
+            for (int j = 0; j < size-1 ; j++) {
+                if ( nums[j] > nums[j + 1]){
+                    temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("\n");
+
+        System.out.println("After sorting");
+        for (int num : nums) {
+            System.out.print(num + ", ");
+        }
+
+    }
+}
